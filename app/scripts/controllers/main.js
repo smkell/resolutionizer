a6049b6769b40a85fac438d5146edfc62cca12e6
Sean Kell <sean.michael.kell@gmail.com>
2014-12-27 14:59:21 -0500

    Move feature list to controller.
    Makes it easier to add new features to list and format as neccesary.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,26 @@
  * Controller of the resolutionizerApp
  */
 angular.module('resolutionizerApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope) {
+        $scope.features = [
+            {
+                feature: 'Guided goal setting',
+                paragraphs: [
+                    'The key to meeting goals is to clearly define what you want to accomplish, why you want to accomplish it, and how you will achieve it.',
+                    'Resolutionizer provides you with simple to understand steps for defining your goals.'
+                ]
+            },
+            {
+                feature: 'Effortless tracking',
+                paragraphs:[
+                    'By linking your existing tracking services with your Resolutionizer, we will automatically update your progress based off of your existing habits.'
+                ]
+            },
+            {
+                feature: 'Beautiful dashboard',
+                paragraphs: [
+                    'As you work to achive your goals you\'ll be presented with beautifulvisualizations of your progress.'
+                ]
+            }
+        ];
+    });
