d8338a69545b2d5e08c3a230fab81f2450fd60df
Sean Kell <sean.michael.kell@gmail.com>
2014-12-28 18:07:49 -0500

    Flesh out contacts page.

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+* @ngdoc function
+* @name resolutionizerApp.controller:ContactCtrl
+* @description
+* # ContactCtrl
+* Controller of the resolutionizerApp
+*/
+angular.module('resolutionizerApp')
+	.controller('ContactCtrl', function () {
+	});
